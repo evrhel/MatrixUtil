925cfc6867495188863b138162f08200513a5cd9
Ethan Vrhel <ethanvrhel@gmail.com>
2024-05-08 17:24:00 -0700

    Disabled some compiler warnings on MSVC

--- MatrixUtil/include/mutil/mutil.h
+++ MatrixUtil/include/mutil/mutil.h
@@ -2,6 +2,12 @@
 
 // This is the only header that should be included by the user.
 
+// disable unitialized variable warning for the entire library
+#if _WIN32
+#pragma warning(push)
+#pragma warning(disable : 26495)
+#endif
+
 #include "settings.h"
 
 #include "math/math.h"
@@ -12,3 +18,7 @@
 #include "vec/vec_impl.h"
 #include "mat/mat_impl.h"
 #include "quat/quaternion_impl.h"
+
+#if _WIN32
+#pragma warning(pop)
+#endif
